4a6b9b0efcca78299e0504e1ad6f4ba6d0246e41
ben <3290701522@qq.com>
2023-07-20 20:27:23 +0800

    优化print

--- chap1/comprehension.py
+++ chap1/comprehension.py
@@ -1,5 +1,6 @@
 from stopwatch import StopWatch
 
+# literal
 alist = [1,2,3,4]
 atuple = (1,2,3,4)
 adict = {'a':1, 'b':2, 'c':3, 'd':4}
@@ -10,24 +11,29 @@ a_new_list_ugly = []
 for i in alist:
     a_new_list_ugly.append(i**2)
 
+print(f'a_new_list_ugly: {a_new_list_ugly}')
+
 a_new_list = [i**2 for i in alist]
-print(a_new_list)
+print(f'a_new_list: {a_new_list}')
 
 # 元组推导式
 a_new_tuple = tuple(i**2 for i in atuple)
-print(a_new_tuple)
+print(f'a_new_tuple: {a_new_tuple}')
 
 # 生成器
 a_generator = (i**2 for i in atuple)
-print(a_generator)  
+print(f'a_generator: {a_generator}')
 
 # 字典推导式
 a_new_dict = {k:v**2 for k,v in adict.items()}
-print(a_new_dict)
+print(f'a_new_dict: {a_new_dict}')
 
 # 集合推导式
-a_new_set = {i**2 for i in aset}
-print(a_new_set)
+def f(x):
+    return x**2 + 2*x + 1
+
+a_new_set = {f(i) for i in aset}
+print(f'a_new_set: {a_new_set}')
 
 
 # benchmark
@@ -38,10 +44,10 @@ for i in range(100_000):
     for i in alist:
         a_new_list_ugly.append(i**2)
 stopwatch.stop()
-print(stopwatch.getElapsedTime())
+print(f'a_new_list_ugly: {stopwatch.getElapsedTime()} ms')
 
 stopwatch.start()
 for i in range(100_000):
     a_new_list = [i**2 for i in alist]
 stopwatch.stop()
-print(stopwatch.getElapsedTime())
+print(f'a_new_list: {stopwatch.getElapsedTime()} ms')
